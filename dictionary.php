830aa9744571fd0217eee09bfc7d390212520352
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2011-10-25 12:12:28 +0200

    Adding terms to translate

--- dictionary.php
+++ dictionary.php
@@ -1,7 +1,7 @@
 <?php
 $LANG=array(
     "about" => "About DiscoJuice",
-    "aboutDescr" => 'DiscoJuice is a user interface to help users select which provider to login with. DiscoJuice is created by <a href="http =>//uninett.no">UNINETT</a>',
+    "aboutDescr" => 'DiscoJuice is a user interface to help users select which provider to login with. DiscoJuice is created by %sUNINETT%s',
     "aboutMore" => "Read more about DiscoJuice",
     "loading" => "Loading list of providers",
     "orSearch" => "or search for a provider, such as Example University",
