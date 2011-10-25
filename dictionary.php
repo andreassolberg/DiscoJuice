92a955d899a1c69f3ed8b54ac2205a5a156b8295
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2011-10-25 11:21:39 +0200

    adding phph dictionary

---
+++ dictionary.php
@@ -0,0 +1,10 @@
+<?php
+$LANG=array(
+    "about" => "About DiscoJuice",
+    "aboutDescr" => 'DiscoJuice is a user interface to help users select which provider to login with. DiscoJuice is created by <a href="http =>//uninett.no">UNINETT</a>',
+    "aboutMore" => "Read more about DiscoJuice",
+    "loading" => "Loading list of providers",
+    "orSearch" => "or search for a provider, such as Example University",
+    "pageReturn" => "Return to list of providers",
+);
+?>
